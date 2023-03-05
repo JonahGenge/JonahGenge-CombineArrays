00489739136096a3294599f6bb53f4ecf80917eb
JonahGenge <jonah.genge@yahoo.com>
2023-03-05 21:02:29 +0000

    done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int newLength = arr1.length + arr2.length;
+        int[] merged = new int[newLength];
+        int position = 0;
+        for(int i : arr1){
+            merged[position] = i;
+            position++;
+        }
+        for(int j : arr2){
+            merged[position] = j;
+            position++;
+        }
 
-        return null;
+        return merged;
     }
 }
